0874ee6cb5bfcc7e8713e64555d75d787a2cb74c
Nathan Etave <nathan.etave@gmail.com>
2025-02-04 15:19:31 +0100

     

--- apps/realtime-elo-ranker-server/test/matches.e2e-spec.ts
+++ apps/realtime-elo-ranker-server/test/matches.e2e-spec.ts
@@ -3,8 +3,9 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { App } from 'supertest/types';
 import { AppModule } from './../src/app.module';
+import { v4 } from 'uuid';
 
-describe('AppController (e2e)', () => {
+describe('MatchesController (e2e)', () => {
   let app: INestApplication<App>;
 
   beforeEach(async () => {
@@ -16,10 +17,5 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
-  });
+  it('/match (POST)', () => {});
 });
